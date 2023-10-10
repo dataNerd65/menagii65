da5cc6b0119c4daec5738e8881d8026c33bd6bd5
Peter Kiarie <114004785+dataNerd65@users.noreply.github.com>
2023-10-10 12:20:54 +0300

    Add files via upload

---
+++ myQuadAPP.py.py
@@ -0,0 +1,29 @@
+a = float(input("Enter a:"))
+b = float(input("Enter b:"))
+c = float(input("Enter c:"))
+
+print(a)
+print(b)
+print(c)
+
+import math
+
+
+p = math.sqrt((b * b) - (4 * a * c))
+
+soln1 = ((-b + p) / (2 * a))
+soln2 = ((-b - p) / (2 * a))
+
+print(f"The first solution is {soln1}.")
+
+print(f"The second solution is {soln2}.")
+
+
+
+
+
+
+
+
+
+
